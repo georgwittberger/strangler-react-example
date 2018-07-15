e0e418cf4662cbbc04d87dd36f21ce71eb142ac3
Georg Wittberger <georg.wittberger@gmail.com>
2018-07-15 21:12:51 +0200

    Let page server control server-side rendering

--- server/page-server.js
+++ server/page-server.js
@@ -1,11 +1,21 @@
 import path from 'path';
 import express from 'express';
+import jsonfile from 'jsonfile';
 
 const server = express();
 const port = process.env.PORT || 4000;
+const serverSideRenderPaths = jsonfile
+  .readFileSync(path.resolve(__dirname, 'server-side-render-paths.json'))
+  .map((path) => new RegExp(path));
 
 server.listen(port, () => {
   console.info(`✔️ Page server is listening. Visit: 🔗 http://localhost:${port}/`);
+});
+
+// Set special response header to enable server-side rendering of React components.
+server.use(serverSideRenderPaths, (req, res, next) => {
+  res.setHeader('x-react-server-side-render', 'true');
+  next();
 });
 
 // Simply serve the content of the "build" directory as static resources.
